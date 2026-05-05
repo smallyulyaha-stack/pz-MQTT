9845cda8ae8d0d36565ad5f39bd47777521c5106
smallyulyaha-stack <smallyulyaha@gmail.com>
2026-05-06 00:26:58 +0300

    Create test MQTT on PS script
    Add PowerShell script to test MQTT publishing

---
+++ broker/test MQTT on PS.ps1
@@ -0,0 +1,4 @@
+ cd "C:\Program Files\mosquitto"
+PS C:\Program Files\mosquitto> .\mosquitto_pub.exe -t "university/test" -m "Привіт з Windows!"
+PS C:\Program Files\mosquitto> .\mosquitto_pub.exe -t "university/test" -m "Hello Yuliya from Windows!"
+PS C:\Program Files\mosquitto> .\mosquitto_pub.exe -t "university/test" -m "test testoviy"
