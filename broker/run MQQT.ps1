4608c57e7e65ff7cc8af830aaf1475cd45ae19ff
smallyulyaha-stack <smallyulyaha@gmail.com>
2026-05-06 00:27:57 +0300

    Update run MQQT.ps1

--- broker/run MQQT.ps1
+++ broker/run MQQT.ps1
@@ -1,7 +1,9 @@
-
-Copyright (C) Microsoft Corporation. All rights reserved.
-
-Install the latest PowerShell for new features and improvements! https://aka.ms/PSWindows
-
 PS C:\WINDOWS\system32> cd "C:\Program Files\mosquitto"
 PS C:\Program Files\mosquitto> .\mosquitto_sub.exe -t "university/test"
+╧Ёшт│Є ч Windows!
+Hello Yuliya from Windows!
+Hello from Postman!
+test testoviy
+Hello from Postman! to PS
+hello wrom PS to postman
+test
