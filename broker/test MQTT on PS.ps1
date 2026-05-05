a6333e66b0e2b3b67aaddd1ac71a9829b5117f13
smallyulyaha-stack <smallyulyaha@gmail.com>
2026-05-06 00:28:21 +0300

    Update test MQTT on PS.ps1

--- broker/test MQTT on PS.ps1
+++ broker/test MQTT on PS.ps1
@@ -1,4 +1,8 @@
- cd "C:\Program Files\mosquitto"
+PS C:\WINDOWS\system32> cd "C:\Program Files\mosquitto"
 PS C:\Program Files\mosquitto> .\mosquitto_pub.exe -t "university/test" -m "Привіт з Windows!"
 PS C:\Program Files\mosquitto> .\mosquitto_pub.exe -t "university/test" -m "Hello Yuliya from Windows!"
 PS C:\Program Files\mosquitto> .\mosquitto_pub.exe -t "university/test" -m "test testoviy"
+PS C:\Program Files\mosquitto> .\mosquitto_pub.exe -t "university/test" -m "hello wrom PS to postman"
+PS C:\Program Files\mosquitto> .\mosquitto_pub.exe -t "university/test" -m "test"
+PS C:\Program Files\mosquitto> .\mosquitto_pub.exe -t "university/test" -m "test to postman"
+PS C:\Program Files\mosquitto>
